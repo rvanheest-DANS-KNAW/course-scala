bd2dd468c714a77938c762b388dce7155df9d346
Richard van Heest <A.w.m.vanHeest@student.tudelft.nl>
2016-03-06 13:11:06 +0100

    Moved and extended traits.sc

---
+++ src/main/scala/traitAbstractClassesAndTypes/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * In this second workshop we will cover the following topics:
+ * <ul>
+ *     <li>traits</li>
+ *     <li>abstract classes</li>
+ *     <li>type aliasses</li>
+ * </ul>
+ */
+package traitAbstractClassesAndTypes;
